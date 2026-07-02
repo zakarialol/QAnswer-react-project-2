fd19acd1f8d47a790e339dc5e21d1093d9dbfd39
zakariaFrontDev <zakaria.aitbibote@gmail.com>
2026-07-02 03:26:32 +0100

    fixing the base path inside vit config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/QAnswer-react-project-2",
+  base: "/QAnswer-react-project-2/",
 })
